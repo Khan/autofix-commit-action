42794c03707623a5c4ad4058f0ab525489534fe2
Jared Forsyth <jared@jaredforsyth.com>
2020-05-19 12:28:48 -0600

    correct location for bash

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require('child_process');
 
 // TODO(jared): Maybe convert the bash file into javascript? Not sure if it's worth it.
-const result = spawnSync('bash', ['./commit_and_push.sh'], {
+const result = spawnSync('bash', [__dirname + '/commit_and_push.sh'], {
     stdio: 'inherit',
     encoding: 'utf8',
 });
