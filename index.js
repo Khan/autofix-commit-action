cc31621e7ef1d5869f01d11626f5a638f0dbd115
Jared Forsyth <jared@jaredforsyth.com>
2020-05-19 12:23:35 -0600

    Add bash file

---
+++ index.js
@@ -0,0 +1,9 @@
+const { spawnSync } = require('child_process');
+
+// TODO(jared): Maybe convert the bash file into javascript? Not sure if it's worth it.
+const result = spawnSync('bash', ['./commit_and_push.sh'], {
+    stdio: 'inherit',
+    encoding: 'utf8',
+});
+
+process.exit(result.status);
